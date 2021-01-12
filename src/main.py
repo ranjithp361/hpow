bbaec52c8ee489b24f13fc6816e2f8f85c9e5b19
Ranjith Puthenmalika <pranjith@icloud.com>
2021-01-11 20:36:25 -0500

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,31 @@
+import random
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import requests
+import config
+import reset
+
+# Authorize Google Sheets
+try:
+	scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+	credentials = ServiceAccountCredentials.from_json_keyfile_name(config.credentials, scope)
+	gc = gspread.authorize(credentials)
+except OSError as e:
+	print("JSON file with Google account credentials not found!")
+	print("Make sure you've followed the README instructions and added the filepath of your credentials file to config.py")
+	exit(1)
+
+# Open sheets
+datasheet = gc.open(config.spreadsheet).worksheet("datasheet")
+
+# Get question and options from current index
+
+pasad_date = datasheet.cell(1, 1).value
+pasad_count = datasheet.cell(1, 2).value
+
+if prasad_date == "":
+	print("No questions found in the Google sheet!")
+		exit(1)
+else
+print("date:" prasad_date)
+print("count:" prasad_count)
