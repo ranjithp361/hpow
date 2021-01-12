27c1d539b92f3437065cef35dc0e74e3834a6fa9
Ranjith Puthenmalika <pranjith@icloud.com>
2021-01-11 22:21:22 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -21,15 +21,15 @@ datasheet = gc.open(config.spreadsheet).worksheet("datasheet")
 # Get question and options from current index
 
 for x in range(2, 50):
-  print(x):
+  print(x) 
 
 if datasheet.cell(x, 1).value > "":
-prasad_date = datasheet.cell(x, 1).value:
-prasad_count = datasheet.cell(x, 2).value:
+prasad_date = datasheet.cell(x, 1).value 
+prasad_count = datasheet.cell(x, 2).value 
 
 # if prasad_date == "":
 #	print("No questions found in the Google sheet!")
 #	exit(1)
 # else
-print("date:", prasad_date):
+print("date:", prasad_date) 
 print("count:", prasad_count)
