0f10145363da331329320eca953131c348b0dc5f
Ranjith Puthenmalika <pranjith@icloud.com>
2021-01-11 20:59:13 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import requests
 import config
-#import reset
+# import reset
 
 # Authorize Google Sheets
 try:
@@ -23,9 +23,9 @@ datasheet = gc.open(config.spreadsheet).worksheet("datasheet")
 prasad_date = datasheet.cell(1, 1).value
 prasad_count = datasheet.cell(1, 2).value
 
-#if prasad_date == "":
+# if prasad_date == "":
 #	print("No questions found in the Google sheet!")
 #	exit(1)
-#else
-#print("date:" prasad_date)
-#print("count:" prasad_count)
+# else
+# print("date:" prasad_date)
+# print("count:" prasad_count)
