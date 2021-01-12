1db3f1de03d0ad7984361babde4151519f18f252
Ranjith Puthenmalika <pranjith@icloud.com>
2021-01-11 22:01:31 -0500

    Update config.py

---
+++ src/config.py
@@ -0,0 +1,11 @@
+# Refer to the instructions in the README
+
+# Required
+# token = "1503587663:AAFSFn0Isbf-K2qD3u5rebMzl8BAxF62l5A"        # Telegram bot token
+credentials = "annadaanamdashboard-c61a1e94c1c4.json"  # Path to credentials JSON file
+spreadsheet = "Annadhanam Dashboard"  # Name of Google Sheet
+#channel = "Dharmabee_2021"      # Unique identifier of your channel/chat
+
+# Optional
+anonymous = True  # Whether to keep  responses anonymous
+loop = True       # Whether to loop questions
